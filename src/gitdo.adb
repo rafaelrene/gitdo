d2d1e0f7cc8389f7a18336be871df4f5bc928da9
René Rafael <rene.rafael@groupsolver.com>
2023-09-09 21:51:10 +0200

    chore(initial commit): List directories in current working directory - Figure out when and how to recurse

---
+++ src/gitdo.adb
@@ -0,0 +1,39 @@
+with Ada.Text_IO;     use Ada;
+with Ada.Directories; use Ada;
+
+procedure Gitdo is
+  use Ada.Directories;
+
+  procedure Find_Files is
+    Dir                    : Directory_Entry_Type;
+    Search                 : Search_Type;
+    Current_Directory_Path : constant String := Current_Directory;
+  begin
+    Start_Search
+     (Search => Search, Directory => Current_Directory_Path, Pattern => "");
+
+    Text_IO.Put_Line (Current_Directory_Path);
+
+    loop
+      exit when not More_Entries (Search);
+
+      Get_Next_Entry (Search, Dir);
+
+      Text_IO.Put_Line (Full_Name (Dir));
+
+      if Kind (Dir) = Ordinary_File then
+        Text_IO.Put_Line ("Ordinary file: " & Full_Name (Dir));
+      end if;
+
+      if Kind (Dir) = Special_File then
+        Text_IO.Put_Line ("Special file: " & Full_Name (Dir));
+      end if;
+
+      if Kind (Dir) = Directory then
+        Text_IO.Put_Line ("Directory: " & Full_Name (Dir));
+      end if;
+    end loop;
+  end Find_Files;
+begin
+  Find_Files;
+end Gitdo;
